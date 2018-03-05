eedec7f04e10195d5d6fd1c75d6bc6afa9a08aa4
workat60474 <workat60474@gmail.com>
2018-03-05 19:26:44 +0800

    Eeduce size of entry2

--- phonebook_opt.c
+++ phonebook_opt.c
@@ -1,16 +1,26 @@
 #include <stdlib.h>
-
+#include <string.h>
 #include "phonebook_opt.h"
 
 /* TODO: FILL YOUR OWN IMPLEMENTATION HERE! */
 entry *findName(char lastName[], entry *pHead)
 {
     /* TODO: implement */
+    while (pHead != NULL) {
+        if (strcasecmp(lastName, pHead->lastName) == 0)
+            return pHead;
+        pHead = pHead->pNext;
+    }
     return NULL;
 }
 
 entry *append(char lastName[], entry *e)
 {
     /* TODO: implement */
+    e->pNext = (entry *) malloc(sizeof(entry));
+    e = e->pNext;
+    strcpy(e->lastName, lastName);
+    e->pNext = NULL;
+    return e;
     return NULL;
 }
